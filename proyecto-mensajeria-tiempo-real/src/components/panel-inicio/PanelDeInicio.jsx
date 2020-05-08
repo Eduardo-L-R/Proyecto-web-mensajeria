9b84d227321e094e7e3e75386e6bfc6612b16927
Eduardo-L-R <e.leiva.rocuant@gmail.com>
2020-05-08 17:07:32 -0400

    Iniciando conexion a firebase desde el componente Panel de inicio, donde se usa hook de efecto para generar el component didmount y obtener info del usuario

--- proyecto-mensajeria-tiempo-real/src/components/panel-inicio/PanelDeInicio.jsx
+++ proyecto-mensajeria-tiempo-real/src/components/panel-inicio/PanelDeInicio.jsx
@@ -1,23 +1,70 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import ChatContainer from './contenedor-chat/ChatContainer'
 import SideBar from './sidebar/SideBar'
 import { makeStyles } from '@material-ui/core/styles';
+import firebase from "firebase";
+import { connect } from "react-redux";
 
-const useStyle = makeStyles({
-  container: {
-    display: "flex",
-    flexFlow: "row wrap"
+export function PanelDeInicio(props){
+
+  useEffect(() => {
+      firebase.firestore().collection("emailUsuarios").doc(props.currentEmail).get().then(function(doc) {
+        if (doc.exists) {
+            console.log("Document data:", doc.data());
+            console.log("Informacion buscada ", doc.data().Nombre);
+        } else {            
+            console.log("No such document!");
+        }
+    }).catch(function(error) {
+        console.log("Error getting document:", error);
+    }); 
+  }, []);
+
+/*     MantenerConectadoChats=(elementosUser)=>{
+      let infoJugador={}
+      console.log(elementosUser);
+      firebase.firestore().collection("Usuarios").doc(elementosUser).onSnapshot(function(doc) {
+        if(doc.exists){infoJugador = doc.data(); console.log(infoJugador);}
+        else{alert("Problemas de conexion, verficar nombre contrincante")}
+      });
+    } */
+  
+    const useStyle = makeStyles({
+      container: {
+        display: "flex",
+        flexFlow: "row wrap"
+      }
+    });
+    const classes = useStyle();
+
+    return (
+      <div className={classes.container}>
+        <SideBar />
+        <ChatContainer />
+      </div>
+    )
   }
-})
 
-function PanelDeInicio() {
-  const classes = useStyle();
-  return (
-    <div className={classes.container}>
-      <SideBar />
-      <ChatContainer />
-    </div>
-  )
-}
 
-export default PanelDeInicio
+
+// Acciones y states de redux importadas
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+    currentEmail: state.user.currentLogin.email
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    // setCurrentRegister: event => dispatch(setCurrentRegister(event)),
+    // setCurrentLogin: event => dispatch(setCurrentLogin(event)),
+    // register: () => dispatch(register()),
+    // signIn: (callback) => dispatch(signIn(callback))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PanelDeInicio);
